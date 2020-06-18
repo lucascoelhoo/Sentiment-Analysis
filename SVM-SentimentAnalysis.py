7d1b7baa6101cd5e88fd6219228a98fa8cfaaf5f
Lucas C <luccoelhoo@gmail.com>
2020-06-18 01:25:58 -0300

    Adicionando arquivos do projeto ao repositório.

---
+++ SVM-SentimentAnalysis.py
@@ -0,0 +1,48 @@
+# Dataset: Polarity dataset v2.0
+# http://www.cs.cornell.edu/people/pabo/movie-review-data/
+#
+# Discussion at https://medium.com/@vasista/sentiment-analysis-textblob-vs-svm-338d418e3ff1
+
+from sklearn.feature_extraction.text import TfidfVectorizer
+import time
+from sklearn import svm
+from sklearn.metrics import classification_report
+import pandas as pd
+
+# train Data
+trainData = pd.read_csv("https://raw.githubusercontent.com/Vasistareddy/sentiment_analysis/master/data/train.csv")
+print(trainData.iloc[:3])
+print("\n\n")
+# test Data
+testData = pd.read_csv("https://raw.githubusercontent.com/Vasistareddy/sentiment_analysis/master/data/test.csv")
+
+# Create feature vectors
+vectorizer = TfidfVectorizer(min_df = 5,
+                             max_df = 0.8,
+                             sublinear_tf = True,
+                             use_idf = True)
+
+train_vectors = vectorizer.fit_transform(trainData['Content'])
+test_vectors = vectorizer.transform(testData['Content'])
+
+print(train_vectors)
+print("\n")
+
+
+
+# Perform classification with SVM, kernel=linear
+classifier_linear = svm.SVC(kernel='polynomial')
+t0 = time.time()
+classifier_linear.fit(train_vectors, trainData['Label'])
+t1 = time.time()
+prediction_linear = classifier_linear.predict(test_vectors)
+t2 = time.time()
+time_linear_train = t1-t0
+time_linear_predict = t2-t1
+
+# results
+print("Results for SVC(kernel=linear)")
+print("Training time: %fs; Prediction time: %fs" % (time_linear_train, time_linear_predict))
+report = classification_report(testData['Label'], prediction_linear, output_dict=True)
+print('positive: ', report['pos'])
+print('negative: ', report['neg'])
